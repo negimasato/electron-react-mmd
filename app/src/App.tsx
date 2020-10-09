e1fc8940a78c1c3a0716985bc5ae1da99851660b
negimasato <nishimasa.subaru@gmail.com>
2020-10-09 17:26:58 +0900

    test

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,42 @@
-import React, { Component }  from 'react';
+import React, { Component, useRef }  from 'react';
 import { Canvas, useFrame } from 'react-three-fiber';
+import * as THREE from 'three'
+import { Mesh,Scene } from 'three';
+import { MMDLoader } from 'three/examples/jsm/loaders/MMDLoader'
 import './App.css';
+
 
 class App extends Component {
 
-  public fileInput:any;
+  public fileInput: React.RefObject<HTMLInputElement>;
+  public scene: Scene;
+  public mesh: any;
 
   constructor(props: any) {
     // highlight-range{3}
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.fileInput = React.createRef();
+    this.fileInput = React.createRef<HTMLInputElement>();
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color( 0xffffff );
   }
 
-  handleSubmit(event: any) {
+  async handleSubmit(event: any) {
     // highlight-range{3}
     event.preventDefault();
-    alert(
-      `Selected file - ${this.fileInput.current.files[0].name}`
-    );
+    if(this.fileInput.current?.files === null) {
+      return;
+    }
+    const url: string = URL.createObjectURL(this.fileInput.current?.files[0]);
+    const loader = new MMDLoader();
+    console.log(url);
+    loader.loadPMX(url, (mmd) => {
+      console.log(mmd)
+    }, (xhr) => {
+      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    },(error) => {
+      console.error(error);
+    });
   }
   
   render(){
@@ -32,6 +50,8 @@ class App extends Component {
           <br />
           <button type="submit">Submit</button>
         </form>
+        <Canvas>
+        </Canvas>
       </div>
     )
   }
